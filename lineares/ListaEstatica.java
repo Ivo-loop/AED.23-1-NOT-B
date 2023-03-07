628d63c89a4d06a0dbe38defb8585bbf4d43c7bc
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2023-03-06 21:05:16 -0300

    Lista estática - lista 1.

--- lineares/ListaEstatica.java
+++ lineares/ListaEstatica.java
@@ -3,12 +3,12 @@ package lineares;
 public class ListaEstatica {
 	private int[] info;
 	private int tamanho;
-	
+
 	public ListaEstatica() {
 		info = new int[10];
 		tamanho = 0;
 	}
-	
+
 	public void inserir(int valor) {
 		if (tamanho == info.length) {
 			this.redimensionar();
@@ -18,33 +18,78 @@ public class ListaEstatica {
 	}
 
 	private void redimensionar() {
-		int[] novo = new int[info.length+10];
-		for (int i=0; i < info.length; i++) {
+		int[] novo = new int[info.length + 10];
+		for (int i = 0; i < info.length; i++) {
 			novo[i] = info[i];
 		}
 		this.info = novo;
 	}
-	
+
 	public int buscar(int valor) {
-		for (int i=0; i < tamanho; i++) {
+		for (int i = 0; i < tamanho; i++) {
 			if (valor == info[i]) {
 				return i;
 			}
 		}
 		return -1;
 	}
-	
+
 	public boolean estaVazia() {
 		return (tamanho == 0);
 	}
-	
+
 	public void retirar(int valor) {
 		int pos = this.buscar(valor);
 		if (pos != -1) { // encontrou o valor
-			for (int i=pos; i < tamanho-1; i++) {
-				info[i] = info[i+1];
+			for (int i = pos; i < tamanho - 1; i++) {
+				info[i] = info[i + 1];
 			}
 			this.tamanho--;
 		}
 	}
+
+	public String exibir() {
+		String str = "[";
+		for (int i = 0; i < tamanho; i++) {
+			str += info[i] + ", ";
+		}
+		return str + "]";
+	}
+
+	public ListaEstatica copiar() { // contribuição Maria Clara
+		ListaEstatica outra = new ListaEstatica();
+		for (int i = 0; i < this.tamanho; i++) {
+			outra.inserir(this.info[i]);
+		}
+
+		return outra;
+	}
+
+	public ListaEstatica dividir() {
+		int metade = this.tamanho / 2;
+		ListaEstatica outra = new ListaEstatica();
+		for (int i = metade; i < this.tamanho; i++) {
+			outra.inserir(this.info[i]);
+		}
+		this.tamanho = metade;
+		return outra;
+	}
+
+	public int getTamanho() {
+		return this.tamanho;
+	}
+
+	public void concatenar(ListaEstatica outraLista) {
+		for (int i = 0; i < outraLista.getTamanho(); i++) {
+			this.inserir(outraLista.pegar(i));
+		}
+	}
+
+	public int pegar(int pos) {
+		if (pos >= this.tamanho) {
+			throw new IndexOutOfBoundsException("Pos="+pos+". Length="+tamanho);
+		}
+		return this.info[pos];
+	}
+
 }
